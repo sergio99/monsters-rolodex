e54b766382ba8058f6a103c0cea581363e9a84e1
sergio99 <sdrodriguez92@gmail.com>
2020-06-07 13:28:53 -0600

    Adding files for github pages

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",   
+     };
+      // this.handleChange=this.handleChange.bind(this);//pass the global 'this'
+
+      // monsters: [
+      //   {
+      //     name:'Frankestein',
+      //     id:'m1'
+      //   },
+      //   {
+      //     name:'Dracula',
+      //     id:'m2'
+      //   },
+      //   {
+      //     name:'Zombie',
+      //     id:'m3'
+      //   }
+      // ]
+
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange=(e)=>{
+    this.setState({ searchField: e.target.value })
+  }
+  render() {
+    const {monsters, searchField}=this.state;
+    const filteredMonsters=monsters.filter(monster=>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+        //  <SearchBox placeholder='search monsters' handleChange={e => this.setState({ searchField: e.target.value })}/>
+    return (
+      <div className="App">  
+           <h1>Monster Rolodex</h1>
+        <SearchBox placeholder='search monsters' handleChange={this.handleChange }/>
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
+  }
+}
+export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello my name is Chira</p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
