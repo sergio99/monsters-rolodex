a0895a6c6dc52dfdb614d55c82e05e740e2d70bc
sergio99 <sdrodriguez92@gmail.com>
2020-06-07 13:26:47 -0600

    Updates

---
+++ precache-manifest.4f9e2fd9016e0286a1e7eb68e362bdd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d286b8726bf30ea467d0832fa047c6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2c637c95684481ea2970",
+    "url": "/monsters-rolodex/static/css/main.7f851ada.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c637c95684481ea2970",
+    "url": "/monsters-rolodex/static/js/main.2a0790b9.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
